1307340c40288245078fc9e60b70cee07b933a31
Zkaria <ZkariaDiab308>
2026-03-09 00:37:58 +0100

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by Zkaria Diab on 2026-03-09.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
